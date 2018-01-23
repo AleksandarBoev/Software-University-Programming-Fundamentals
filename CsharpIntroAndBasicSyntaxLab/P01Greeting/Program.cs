02612ea1d8ab179654a57462b8715de339fc49dc
AleksandarBoev <33186527+AleksandarBoev@users.noreply.github.com>
2018-01-23 20:16:29 +0200

    Added lab

---
+++ CsharpIntroAndBasicSyntaxLab/P01Greeting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
