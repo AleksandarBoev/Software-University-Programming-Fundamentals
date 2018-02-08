0cba921e855034da23a0d03101f7f0bc7fc69c59
AleksandarBoev <33186527+AleksandarBoev@users.noreply.github.com>
2018-02-08 22:57:08 +0200

    Commit

--- ListsExercises/P01MaxSequenceOfEqualElements/Program.cs
+++ ListsExercises/P01MaxSequenceOfEqualElements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P01MaxSequenceOfEqualElements
 {
@@ -6,7 +8,40 @@ namespace P01MaxSequenceOfEqualElements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> listOfNumbers = new List<int>();
+            listOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int bestStart = 0;
+            int currentStart = 0;
+            int currentLength = 1;
+            int bestLength = 1;
+
+            for (int index = 0; index < listOfNumbers.Count - 1; index++)
+            {
+                if (listOfNumbers[index] == listOfNumbers[index + 1])
+                {
+                    currentLength++;
+
+                    if (currentLength > bestLength)
+                    {
+                        bestStart = currentStart; // currentStart ne se promenq, tui che bestStart se prisvoqva nqkolko puti
+                        bestLength = currentLength; 
+                    }
+                }
+                else
+                {
+                    currentLength = 1; // vinagi shte e 1. Nqma kak edin elemnt da se povtarq 0 puti sthom go ima
+                    currentStart = index + 1; //shtom segashniq indeks ne stava, zapomnq se sledvashtiq
+                }
+
+            }
+
+            for (int index = bestStart; index < bestStart + bestLength; index++)
+            {
+                Console.Write(listOfNumbers[index] + " ");
+            }
+
+            //main ends here
         }
     }
 }
---
+++ ListsExercises/P02ChangeList/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P02ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            string command = "";
+
+            while (command != "Odd" && command != "Even")
+            {
+                string[] userInput = Console.ReadLine().Split(' ').ToArray();
+                command = userInput[0]; //vinagi purviq element na masiva e komandata
+                
+                switch (command)
+                {
+                    case "Delete":
+                        int deleteThisValue = int.Parse(userInput[1]);
+                        while (numbers.Remove(deleteThisValue))
+                        {
+
+                        }
+                            break;
+                    case "Insert":
+                        int index = int.Parse(userInput[1]);
+                        int value = int.Parse(userInput[2]);
+                        numbers.Insert(index, value);
+                        break;
+
+                    case "Odd":
+                        foreach (var item in numbers)
+                        {
+                            if (item % 2 == 1)
+                            {
+                                Console.Write(item + " ");
+                            }
+                        }
+                        break;
+                    case "Even":
+                        foreach (var item in numbers)
+                        {
+                            if (item % 2 == 1)
+                            {
+                                Console.Write(item + " ");
+                            }
+                        }
+                        break;
+                }
+
+                
+            }
+
+            //main ends here
+        }
+
+
+        //class ends here
+    }
+}
