ae338fdb96e112cca97b0c95c3b4d491f0c1bd4b
AleksandarBoev <33186527+AleksandarBoev@users.noreply.github.com>
2018-02-12 22:41:34 +0200

    Commit

---
+++ DictionariesLambdaAndLINQLab/P01CountRealNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, int> doubleCount = new SortedDictionary<double, int>();
+            double[] doublesInput = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            foreach (double value in doublesInput)
+            {
+                if (doubleCount.ContainsKey(value) == false)
+                {
+                    doubleCount.Add(value, 1);
+                }
+                else
+                {
+                    doubleCount[value] = doubleCount[value] + 1;
+                }
+            }
+
+            foreach (var kvp in doubleCount)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+
+            //main ends here
+        }
+    }
+}
---
+++ DictionariesLambdaAndLINQLab/P02OddOccurrences/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string lowerCase = input[i].ToLower();
+                if (wordCount.ContainsKey(lowerCase) == false)
+                {
+                    wordCount.Add(lowerCase, 1);
+                }
+                else
+                {
+                    wordCount[lowerCase]++;
+                }
+            }
+
+            string answer = "";
+            foreach (var kvp in wordCount)
+            {
+                if (kvp.Value % 2 == 1)
+                {
+                    answer += kvp.Key + ", ";
+                }
+            }
+
+            answer = answer.Trim(',', ' ');
+            Console.WriteLine(answer);
+
+            //main ends here
+        }
+    }
+}
