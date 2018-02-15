5951df3f89787f951488d553c98e230b2b9317e3
AleksandarBoev <aleksandarboev95@gmail.com>
2018-02-15 16:50:00 +0200

    Created files

---
+++ ObjectsAndClassesExercises/P01CountWorkingDays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01CountWorkingDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
