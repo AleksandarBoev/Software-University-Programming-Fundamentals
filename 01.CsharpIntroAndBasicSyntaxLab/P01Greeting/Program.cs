9dd04aba77594ef8a3d0f938674e354402b564ce
AleksandarBoev <aleksandarboev95@gmail.com>
2018-04-25 21:33:04 +0300

    Numbered the lessons

---
+++ 01.CsharpIntroAndBasicSyntaxLab/P01Greeting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P01Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+
+            //main ends here
+        }
+    }
+}
